1b0336500c536834bd436227c794398a5c52bca7
Anna <admin@lensesrequired.com>
2025-05-15 16:27:55 -0600

    Copy, delete, and loading states for Invite codes

--- app/api/watchlist/[id]/invite/route.ts
+++ app/api/watchlist/[id]/invite/route.ts
@@ -5,7 +5,11 @@ import {
   simplifyItem,
 } from '@/server/dynamodb';
 import { checkHasAccess, createInviteCode } from '@/server/watchlist';
-import { PutItemCommand, QueryCommand } from '@aws-sdk/client-dynamodb';
+import {
+  DeleteItemCommand,
+  PutItemCommand,
+  QueryCommand,
+} from '@aws-sdk/client-dynamodb';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(
@@ -143,10 +147,13 @@ export async function POST(
     );
 
     if (inviteCodeResp.Items && inviteCodeResp.Items.length === 10) {
-      return NextResponse.json({
-        _message:
-          'The maximum number of invite codes have already been created',
-      });
+      return NextResponse.json(
+        {
+          _message:
+            'The maximum number of invite codes have already been created',
+        },
+        { status: 400 },
+      );
     }
   } catch (err) {
     console.log('invite code lookup', err);
@@ -188,4 +195,71 @@ export async function POST(
         { status: 500 },
       );
     });
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  const { username } = JSON.parse(
+    decodeURIComponent(request.cookies.get('info')?.value || '{}'),
+  );
+  const { id } = await params;
+  const code = request.nextUrl.searchParams.get('code');
+
+  const watchlistResp = await checkHasAccess(id, username);
+  if (!watchlistResp) {
+    return NextResponse.json(
+      { _message: 'Watchlist does not exist or you do not have access' },
+      { status: 403 },
+    );
+  }
+
+  const [watchlist] = watchlistResp.Items || [];
+  const managedBy = simplify(watchlist.managedBy).replace('USER#', '');
+  if (managedBy !== username) {
+    const managedListResp = await checkHasAccess(id, managedBy);
+    if (managedListResp) {
+      const allowInvites = simplify(
+        (managedListResp.Items || [])[0].allowInvites,
+      );
+      if (!allowInvites) {
+        return NextResponse.json(
+          {
+            _message:
+              'You do not have permission to delete invite codes for this watchlist',
+          },
+          { status: 403 },
+        );
+      }
+    } else {
+      return NextResponse.json(
+        {
+          _message: 'Something went wrong. Please try again',
+        },
+        { status: 500 },
+      );
+    }
+  }
+
+  try {
+    await dbclient.send(
+      new DeleteItemCommand(
+        createParams({
+          Key: {
+            PK: { S: `LIST#${id}` },
+            SK: { S: `INVITE_CODE#${code}` },
+          },
+        }),
+      ),
+    );
+
+    return NextResponse.json({ success: true });
+  } catch (err) {
+    console.log('invite code delete', err);
+    return NextResponse.json(
+      { _message: 'Something went wrong. Please try again later' },
+      { status: 500 },
+    );
+  }
 }
--- components/watchlist/InviteCodeModal.tsx
+++ components/watchlist/InviteCodeModal.tsx
@@ -12,11 +12,13 @@ import {
   DialogContent,
   DialogTitle,
   IconButton,
+  Skeleton,
   Table,
   TableBody,
   TableCell,
   TableHead,
   TableRow,
+  Tooltip,
 } from '@mui/material';
 
 export type InviteCodeModalProps = {
@@ -30,8 +32,13 @@ export const InviteCodeModal = ({
 }: InviteCodeModalProps) => {
   const [error, setError] = useState<string>('');
   const [codes, setCodes] = useState<InviteCode[]>([]);
+  const [copied, setCopied] = useState<number>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [addLoading, setAddLoading] = useState<boolean>(false);
+  const [deleteLoading, setDeleteLoading] = useState<number>();
 
   const loadCodes = () => {
+    setLoading(true);
     apiFetch(`/api/watchlist/${watchlistId}/invite`).then(
       ({ ok, data, error }) => {
         if (ok && data.codes) {
@@ -39,12 +46,13 @@ export const InviteCodeModal = ({
         } else {
           setError(error || 'Something went wrong. Please try again.');
         }
+        setLoading(false);
       },
     );
   };
 
   const addCode = () => {
-    // TODO: loading state
+    setAddLoading(true);
     apiFetch(`/api/watchlist/${watchlistId}/invite`, { method: 'POST' }).then(
       ({ ok, data, error }) => {
         if (ok && data.code) {
@@ -52,8 +60,23 @@ export const InviteCodeModal = ({
         } else {
           setError(error || 'Something went wrong. Please try again.');
         }
+        setAddLoading(false);
       },
     );
+  };
+
+  const deleteCode = (code: string, index: number) => () => {
+    setDeleteLoading(index);
+    apiFetch(`/api/watchlist/${watchlistId}/invite?code=${code}`, {
+      method: 'DELETE',
+    }).then(({ ok, data, error }) => {
+      if (ok && data.success) {
+        loadCodes();
+      } else {
+        setError(error || 'Something went wrong. Please try again.');
+      }
+      setDeleteLoading(undefined);
+    });
   };
 
   useEffect(() => {
@@ -61,6 +84,16 @@ export const InviteCodeModal = ({
     // just on initial load
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const onCopy = (code: string, index: number) => () => {
+    navigator.clipboard.writeText(
+      `https://localhost:3000/watchlist/join?${code}`,
+    );
+    setCopied(index);
+    setTimeout(() => {
+      setCopied(undefined);
+    }, 1000);
+  };
 
   return (
     <Dialog open onClose={onClose} aria-labelledby="create-dialog-title">
@@ -77,56 +110,73 @@ export const InviteCodeModal = ({
           {error && <Alert severity="error">{error}</Alert>}
           Invite Codes are one time use and last for a week before expiring. Use
           the Copy button to get full invite links.
-          {codes.length === 0 ? (
-            <div>No codes</div>
-          ) : (
-            <Table sx={{ minWidth: 400 }} aria-label="invite code table">
-              <TableHead>
-                <TableRow>
-                  <TableCell></TableCell>
-                  <TableCell>Code</TableCell>
-                  <TableCell>Expires</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {codes.map(({ code, expiresAt }, index) => (
-                  <TableRow
-                    key={`code-${index}`}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                  >
-                    <TableCell align="right">
-                      <Box sx={{ minWidth: '80px' }}>
-                        <IconButton color="error">
-                          <DeleteIcon />
-                        </IconButton>
-                        <IconButton>
-                          <ContentCopyIcon fontSize="small" />
-                        </IconButton>
-                      </Box>
-                    </TableCell>
-                    <TableCell
-                      component="th"
-                      scope="row"
-                      sx={{
-                        textOverflow: 'ellipsis',
-                        overflow: 'hidden',
-                        whiteSpace: 'nowrap',
-                      }}
+          <Table sx={{ minWidth: 400 }} aria-label="invite code table">
+            <TableHead>
+              <TableRow>
+                <TableCell></TableCell>
+                <TableCell>Code</TableCell>
+                <TableCell>Expires</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {loading
+                ? Array(codes.length || 1).fill(
+                    <TableRow>
+                      <TableCell>
+                        <Skeleton />
+                      </TableCell>
+                      <TableCell>
+                        <Skeleton />
+                      </TableCell>
+                      <TableCell>
+                        <Skeleton />
+                      </TableCell>
+                    </TableRow>,
+                  )
+                : codes.map(({ code, expiresAt }, index) => (
+                    <TableRow
+                      key={`code-${index}`}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                     >
-                      {code}
-                    </TableCell>
-                    <TableCell>
-                      {new Date(expiresAt).toLocaleDateString()}
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          )}
+                      <TableCell align="right">
+                        <Box sx={{ minWidth: '80px' }}>
+                          <IconButton
+                            color="error"
+                            onClick={deleteCode(code, index)}
+                            loading={deleteLoading === index}
+                          >
+                            <DeleteIcon />
+                          </IconButton>
+                          <Tooltip title="Copied" open={copied === index}>
+                            <IconButton onClick={onCopy(code, index)}>
+                              <ContentCopyIcon fontSize="small" />
+                            </IconButton>
+                          </Tooltip>
+                        </Box>
+                      </TableCell>
+                      <TableCell
+                        component="th"
+                        scope="row"
+                        sx={{
+                          textOverflow: 'ellipsis',
+                          overflow: 'hidden',
+                          whiteSpace: 'nowrap',
+                        }}
+                      >
+                        {code}
+                      </TableCell>
+                      <TableCell>
+                        {new Date(expiresAt).toLocaleDateString()}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+            </TableBody>
+          </Table>
+          {!loading && codes.length === 0 && 'No codes...'}
         </Box>
       </DialogContent>
       <DialogActions>
-        <Button variant="contained" onClick={addCode}>
+        <Button variant="contained" onClick={addCode} loading={addLoading}>
           Add Code
         </Button>
         <Button onClick={onClose}>Close</Button>
