28ca9706d69417b2fb63f589896ba051bf88b45d
Anna <admin@lensesrequired.com>
2025-06-22 09:37:53 -0600

    fix auth form

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -10,6 +10,7 @@ type AuthProps = {
 };
 
 export const Auth = ({ createAccount, redirectTo }: AuthProps) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [email, setEmail] = useState<string>('');
@@ -39,27 +40,33 @@ export const Auth = ({ createAccount, redirectTo }: AuthProps) => {
     }
   }, [usernameError, isValid, emailError]);
 
-  const onLogin = () => {
-    setSubmitError('');
-    if (isValid()) {
-      const body: Record<string, string> = { username, password };
-      if (createAccount) {
-        body.email = email;
-      }
-      apiFetch(createAccount ? '/api/user' : '/api/authenticate', {
-        method: 'POST',
-        body: JSON.stringify(body),
-      }).then(({ ok, data, error }) => {
-        if (ok && data.authed) {
-          if (createAccount) {
-            window.location.assign(decodeURIComponent(redirectTo || '') || '/');
-          } else {
-            window.location.reload();
-          }
-        } else {
-          setSubmitError(error || 'Something went wrong. Please try again.');
+  const onLogin = (e) => {
+    e.preventDefault();
+    if (!isLoading) {
+      setIsLoading(true);
+      setSubmitError('');
+      if (isValid()) {
+        const body: Record<string, string> = { username, password };
+        if (createAccount) {
+          body.email = email;
         }
-      });
+        apiFetch(createAccount ? '/api/user' : '/api/authenticate', {
+          method: 'POST',
+          body: JSON.stringify(body),
+        }).then(({ ok, data, error }) => {
+          if (ok && data.authed) {
+            if (createAccount) {
+              window.location.assign(
+                decodeURIComponent(redirectTo || '') || '/',
+              );
+            } else {
+              window.location.reload();
+            }
+          } else {
+            setSubmitError(error || 'Something went wrong. Please try again.');
+          }
+        });
+      }
     }
   };
 
@@ -74,60 +81,61 @@ export const Auth = ({ createAccount, redirectTo }: AuthProps) => {
   };
 
   return (
-    <Box
-      sx={{
-        height: '75vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        alignContent: 'stretch',
-      }}
-    >
+    <form onSubmit={onLogin}>
       <Box
-        sx={{ display: 'grid', gap: '1rem', flexGrow: 1, maxWidth: '30rem' }}
+        sx={{
+          height: '75vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          alignContent: 'stretch',
+        }}
       >
-        {submitError && <Alert severity="error">{submitError}</Alert>}
-        {createAccount && (
-          <TextField
-            error={!!emailError}
-            id="email"
-            label="Email"
-            variant="outlined"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            helperText={emailError}
-          />
-        )}
-        <TextField
-          error={!!usernameError}
-          id="username"
-          label="Username"
-          variant="outlined"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          helperText={usernameError}
-        />
-        <TextField
-          id="password"
-          label="Password"
-          variant="outlined"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button
-          variant="contained"
-          onClick={() => {
-            onLogin();
-          }}
-          disabled={!username || !password || (createAccount && !email)}
+        <Box
+          sx={{ display: 'grid', gap: '1rem', flexGrow: 1, maxWidth: '30rem' }}
         >
-          {createAccount ? 'Create Account' : 'Login'}
-        </Button>
-        <Button variant="outlined" onClick={redirect}>
-          {createAccount ? 'Back to Login' : 'Create Account'}
-        </Button>
+          {submitError && <Alert severity="error">{submitError}</Alert>}
+          {createAccount && (
+            <TextField
+              error={!!emailError}
+              id="email"
+              label="Email"
+              variant="outlined"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              helperText={emailError}
+            />
+          )}
+          <TextField
+            error={!!usernameError}
+            id="username"
+            label="Username"
+            variant="outlined"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            helperText={usernameError}
+          />
+          <TextField
+            id="password"
+            label="Password"
+            variant="outlined"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            variant="contained"
+            type="submit"
+            disabled={!username || !password || (createAccount && !email)}
+            loading={isLoading}
+          >
+            {createAccount ? 'Create Account' : 'Login'}
+          </Button>
+          <Button variant="outlined" onClick={redirect}>
+            {createAccount ? 'Back to Login' : 'Create Account'}
+          </Button>
+        </Box>
       </Box>
-    </Box>
+    </form>
   );
 };
